0b80a10a8586db0c3935c1034fecf96977ca7a90
Shahzad Malik Muzaffar <shahzad.malik.muzaffar@cern.ch>
2025-10-15 13:01:09 +0200

    get one file at a time

--- src/Utils/ajax.js
+++ src/Utils/ajax.js
@@ -27,7 +27,7 @@ function sleep(ms) {
 
 export function getMultipleFiles({ fileUrlList, onSuccessCallback = () => {} }) {
     fileUrlList = Array.isArray(fileUrlList) ? fileUrlList : [];
-    const concurrencyLimit = 3;
+    const concurrencyLimit = 1;
     const delayMs = 100;
     let index = 0;
     let active = 0;
