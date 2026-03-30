0ac6c497a9b32b119431c54ca8be60937ae5e3d2
Rao <rao@hp-800-g9.dyndns.cern.ch>
2026-03-30 15:03:04 +0200

    [Vite]: moved to vite

---
+++ vite.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig, transformWithEsbuild } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [
+    {
+      name: 'treat-js-files-as-jsx',
+      enforce: 'pre',
+      async transform(code, id) {
+        if (!id.match(/src\/.*\.js$/)) {
+          return null;
+        }
+
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic',
+        });
+      },
+    },
+    react(),
+  ],
+  server: {
+    port: 3000,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+});
